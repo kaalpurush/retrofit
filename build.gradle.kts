9b4650b0065c06ab71f55a43c623795eff032d3d
Ariful Islam <ariful@portonics.com>
2021-05-22 15:35:57 +0600

    gradle cleanups

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+
+    repositories {
+        google()
+        jcenter()
+
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:4.2.1")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:${findProperty("kotlin_version")}")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven {url = uri( "https://dl.bintray.com/kotlin/ktor") }
+    }
+}
+
+tasks.register("clean",Delete::class){
+    delete(rootProject.buildDir)
+}
