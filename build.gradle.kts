c31ddb680cf0e05df007bbf90fcab37414447dfb
Ariful Islam <arifulbd@gmail.com>
2022-06-29 00:28:05 +0600

    dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-
     repositories {
         google()
+        mavenCentral()
         jcenter()
-
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.1.2")
+        classpath ("com.android.tools.build:gradle:7.1.3")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:${findProperty("kotlin_version")}")
-        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.41")
 
     }
@@ -19,6 +18,7 @@ buildscript {
 allprojects {
     repositories {
         google()
+        mavenCentral()
         jcenter()
     }
 }
